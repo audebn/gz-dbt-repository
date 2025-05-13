e5dacde67f0604fc9109a9237dc27392c7c6f5b6
Aude Segala <segala.aude@gmail.com>
2025-05-13 09:40:45 +0000

    shipping_fee

---
+++ models/stg_raw_product.sql
@@ -0,0 +1,12 @@
+with source as (
+  select * from {{ source('raw', 'product') }}
+),
+
+renamed as (
+  select
+    product_id,
+    cast(purchSE_PRICE as FLOAT64)  as purchase_price
+  from source
+)
+
+select * from renamed
