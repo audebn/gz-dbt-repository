48876948915e6c981ab4701a6aac1ab2f7c9f44b
Aude Segala <segala.aude@gmail.com>
2025-05-13 16:51:28 +0000

    operational margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,9 @@
 SELECT 
 s.orders_id,
 date_date,
+shipping_fee,
+logCost, 
+ship_cost,
 margin + shipping_fee - logCost- ship_cost AS operational_margin
 FROM {{ ref("stg_raw_ship") }} AS s
 JOIN {{ref("int_orders_margin")}} AS m 
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -3,13 +3,13 @@ SELECT
   COUNT(DISTINCT s.orders_id) AS nb_transactions,
   SUM(s.revenue) AS total_revenue,
   SUM(s.revenue) / COUNT(DISTINCT s.orders_id) AS average_basket,
-  SUM(s.operational_margin) AS total_operational_margin,
+  SUM(ship.operational_margin) AS total_operational_margin,
   SUM(s.purchase_cost) AS total_purchase_cost,
   SUM(ship.shipping_fee) AS total_shipping_fee,
   SUM(ship.logCost) AS total_log_costs,
   SUM(s.quantity) AS total_products_sold
-FROM {{ ref("stg_sales") }} AS s
-JOIN {{ ref("stg_raw_ship") }} AS ship
+FROM {{ ref("int_sales_margin") }} AS s
+JOIN {{ ref("int_orders_operational") }} AS ship
   ON s.orders_id = ship.orders_id
 GROUP BY s.date_date
 ORDER BY s.date_date DESC
