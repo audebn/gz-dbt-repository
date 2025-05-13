db64078c27c4442c44f899c5c9e7ca48017a0165
Aude Segala <segala.aude@gmail.com>
2025-05-13 14:21:04 +0000

    sales margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -4,7 +4,7 @@ SELECT
   SUM(revenue) AS total_revenue,
   SUM(quantity) AS total_quantity,
   SUM(purchase_cost) AS total_cost,
-  SUM(margin) AS total_margin
+  SUM(margin) AS margin
 FROM {{ ref("int_sales_margin") }} AS sales_margin
 GROUP BY orders_id
 ORDER BY orders_id DESC
---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,10 @@
+SELECT 
+orders_id,
+date_date,
+shipping_fee,
+logCost, 
+ship_cost,
+margin
+FROM {{ ref("stg_raw_ship") }} AS s
+JOIN {{ref("int_orders_margin")}} AS m 
+ON s.orders_id = m.orders_id
