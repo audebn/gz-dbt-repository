ef734e9dd23099008d0ab0dfeb64006fc0073a80
Aude Segala <segala.aude@gmail.com>
2025-05-13 14:00:23 +0000

    purchase cost et margin

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,8 @@
+SELECT
+  s.orders_id,
+  s.date_date,
+  s.revenue,
+  s.quantity,
+  s.quantity * p.purchase_price AS purchase_cost,
+  s.revenue - (s.quantity * p.purchase_price) AS margin
+FROM {{ref("int_sales_margin")}} AS sales_margin
