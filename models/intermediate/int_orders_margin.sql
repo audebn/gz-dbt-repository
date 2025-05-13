8f7709781b5aab6a019ed6ff52bd6fda907765c3
Aude Segala <segala.aude@gmail.com>
2025-05-13 14:07:03 +0000

    purchase cost and margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,9 @@
 SELECT
-  s.orders_id,
-  s.date_date,
-  s.revenue,
-  s.quantity,
-  s.quantity * p.purchase_price AS purchase_cost,
-  s.revenue - (s.quantity * p.purchase_price) AS margin
+  orders_id,
+  date_date,
+  revenue,
+  quantity,
+  purchase_cost,
+  margin
 FROM {{ref("int_sales_margin")}} AS sales_margin
 ORDER BY s.orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,10 @@
 SELECT
-  s.date_date,
-  s.orders_id,
-  s.products_id,
-  s.quantity,
-  s.revenue,
-  p.purchase_price,
+  s.date_date AS date_date,
+  s.orders_id AS orders_od,
+  s.products_id AS products_id,
+  s.quantity AS quantity,
+  s.revenue AS revenue,
+  p.purchase_price AS purchase_price,
   s.quantity * p.purchase_price AS purchase_cost,
   s.revenue - (s.quantity * p.purchase_price) AS margin
 FROM {{ref("stg_sales")}} AS s
